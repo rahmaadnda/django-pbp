2568ad40043ac1c256d10b1148a9ac69b4d96de2
Rahma Adinda Putri <putrirahmaadinda@gmail.com>
2022-09-15 13:18:45 +0700

    menyelesaikan lab 2

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,3 +1,5 @@
+from django.http import HttpResponse
+from django.core import serializers
 from django.shortcuts import render
 from wishlist.models import BarangWishlist
 
@@ -9,3 +11,19 @@ context = {
 
 def show_wishlist(request):
     return render(request, "wishlist.html", context)
+
+data = BarangWishlist.objects.all()
+
+def show_xml(request):
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_json_by_id(request, id):
+    data = BarangWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_by_id(request, id):
+    data = BarangWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
